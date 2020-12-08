c0d7874e194b5bc9647536714664d2f7d34b4653
Katrina <katrina7g@yahoo.com>
2020-12-07 23:20:19 -0800

    Personalize About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -16,11 +16,11 @@ const About: React.FC<PageProps<DataProps>> = ({ data, path }) => (
   <Layout>
     <NavBar />
     <SEO title="About" />
-    <h1>Gatsby supports TypeScript by default!</h1>
-    <p>This means that you can create and write <em>.ts/.tsx</em> files for your pages, components etc. Please note that the <em>gatsby-*.js</em> files (like gatsby-node.js) currently don't support TypeScript yet.</p>
-    <p>For type checking you'll want to install <em>typescript</em> via npm and run <em>tsc --init</em> to create a <em>.tsconfig</em> file.</p>
-    <p>You're currently on the page "{path}" which was built on {data.site.buildTime}.</p>
-    <p>To learn more, head over to our <a href="https://www.gatsbyjs.com/docs/typescript/">documentation about TypeScript</a>.</p>
+    <h1>Katrina Gonzales</h1>
+    <h2>About</h2>
+    <p>This page will have more content in the near future. </p>
+    {/* <p>You're currently on the "{path.replace("\/", "")}" page, which was built on {data.site.buildTime}.</p>
+    <p>*ps: this page is written in TypeScript. See Gatsby <a href="https://www.gatsbyjs.com/docs/typescript/">documentation</a> about TypeScript.</p> */}
   </Layout>
 )
 
